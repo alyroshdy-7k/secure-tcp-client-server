faf8521a81cfbc51cdec8e13743e5d6c60cde614
alyroshdy-7k <ar2400602@tkh.edu.eg>
2026-04-02 22:24:26 +0200

    Update server.cpp

--- milestone1/server.cpp
+++ milestone1/server.cpp
@@ -1,106 +1,85 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <pthread.h>
 #include "security.h"
 
 #define PORT 8080
 #define BUFFER_SIZE 1024
 
-int main()
-{
-    int server_fd, new_socket;
-    struct sockaddr_in address;
-    socklen_t addrlen = sizeof(address);
-
-    char buffer[BUFFER_SIZE] = {0};
-    char received_key[BUFFER_SIZE] = {0};
-
-    char expected_key[] = "secure123";
-    char xor_key[] = "key123";
-
-    char reply[] = "Hello from server";
-    int reply_len = strlen(reply);
-
-    server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd < 0)
-    {
-        printf("Socket creation failed\n");
-        return 1;
+int authenticate(char *user, char *pass) {
+    FILE *file = fopen("users.txt", "r");
+    if (!file) return 0;
+    char f_u[50], f_p[50], f_r[20];
+    while (fscanf(file, "%s %s %s", f_u, f_p, f_r) != EOF) {
+        if (strcmp(user, f_u) == 0 && strcmp(pass, f_p) == 0) {
+            fclose(file);
+            return 1;
+        }
     }
+    fclose(file);
+    return 0;
+}
 
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
+void* Shandle_client(void* arg) {
+    int client_sock = *((int*)arg);
+    free(arg);
+    char buffer[BUFFER_SIZE], user[50], pass[50];
 
-    if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0)
-    {
-        printf("Bind failed\n");
-        close(server_fd);
-        return 1;
-    }
-
-    if (listen(server_fd, 3) < 0)
-    {
-        printf("Listen failed\n");
-        close(server_fd);
-        return 1;
-    }
-
-    printf("Server waiting for client...\n");
-
-    new_socket = accept(server_fd, (struct sockaddr*)&address, &addrlen);
-    if (new_socket < 0)
-    {
-        printf("Accept failed\n");
-        close(server_fd);
-        return 1;
-    }
-
-    printf("Client connected!\n");
-
-    int key_bytes = read(new_socket, received_key, BUFFER_SIZE - 1);
-    if (key_bytes <= 0)
-    {
-        printf("Failed to read authentication key\n");
-        close(new_socket);
-        close(server_fd);
-        return 1;
-    }
-
-    received_key[key_bytes] = '\0';
-
-    if (strcmp(received_key, expected_key) != 0)
-    {
-        printf("Authentication failed\n");
-        close(new_socket);
-        close(server_fd);
-        return 0;
-    }
-
-    printf("Authentication successful!\n");
-    send(new_socket, "AUTH_OK", 8, 0);
-
-    int n = read(new_socket, buffer, BUFFER_SIZE);
-    if (n <= 0)
-    {
-        printf("Failed to read encrypted message\n");
-        close(new_socket);
-        close(server_fd);
-        return 1;
-    }
-
-    printf("Encrypted message received\n");
-
-    xor_encrypt_decrypt(buffer, n, xor_key);
+    int n = read(client_sock, buffer, BUFFER_SIZE);
+    if (n <= 0) { close(client_sock); return NULL; }
     buffer[n] = '\0';
+    sscanf(buffer, "%s %s", user, pass);
 
-    printf("Decrypted message: %s\n", buffer);
+    if (authenticate(user, pass)) {
+        send(client_sock, "AUTH_OK", 7, 0);
+        printf("\n[AUTH] User '%s' logged in on [Socket %d]\n", user, client_sock);
+    } else {
+        send(client_sock, "AUTH_FAIL", 9, 0);
+        close(client_sock);
+        return NULL;
+    }
 
-    xor_encrypt_decrypt(reply, reply_len, xor_key);
-    send(new_socket, reply, reply_len, 0);
+    while ((n = read(client_sock, buffer, BUFFER_SIZE)) > 0) {
+        int aes_len = ((n + 15) / 16) * 16; 
+        unsigned char decrypted[BUFFER_SIZE];
+        memset(decrypted, 0, BUFFER_SIZE); // CLEAN BUFFER TO FIX DIAMOND SYMBOLS
 
-    close(new_socket);
-    close(server_fd);
+        printf("\n[SERVER - Socket %d] RECEIVED ENCRYPTED FROM %s: ", client_sock, user);
+        for(int i=0; i<n; i++) printf("%02x", (unsigned char)buffer[i]);
+
+        aes_decrypt((unsigned char*)buffer, aes_len, decrypted);
+        
+        // Output decrypted message ONLY on server
+        printf("\n[SERVER - Socket %d] DECRYPTED MESSAGE: %s\n", client_sock, decrypted);
+
+        char resp_text[] = "ACK: Message Received";
+        unsigned char encrypted_resp[BUFFER_SIZE] = {0};
+        aes_encrypt((unsigned char*)resp_text, 32, encrypted_resp);
+        send(client_sock, encrypted_resp, 32, 0);
+    }
+
+    close(client_sock);
+    return NULL;
+}
+
+int main() {
+    int s_fd = socket(AF_INET, SOCK_STREAM, 0);
+    int opt = 1;
+    setsockopt(s_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+    struct sockaddr_in addr = {AF_INET, htons(PORT), INADDR_ANY};
+    bind(s_fd, (struct sockaddr*)&addr, sizeof(addr));
+    listen(s_fd, 5);
+    printf("--- MULTI-THREADED AES SERVER ACTIVE ---\n");
+    while (1) {
+        int *c_sock = (int*)malloc(sizeof(int));
+        *c_sock = accept(s_fd, NULL, NULL);
+        printf("[SYSTEM] New connection assigned to [Socket %d]\n", *c_sock);
+        pthread_t t;
+        pthread_create(&t, NULL, handle_client, c_sock);
+        pthread_detach(t);
+    }
     return 0;
 }
