e9c4df10504471c5a977c1386b2178218d74a67f
alyroshdy-7k <ar2400602@tkh.edu.eg>
2026-04-02 22:23:44 +0200

    Update security.cpp

--- milestone1/security.cpp
+++ milestone1/security.cpp
@@ -1,12 +1,17 @@
-#include <string.h>
 #include "security.h"
 
-void xor_encrypt_decrypt(char *data, int len, const char *key)
-{
-    int key_len = strlen(key);
+void aes_encrypt(unsigned char *plaintext, int len, unsigned char *ciphertext) {
+    AES_KEY enc_key;
+    AES_set_encrypt_key((unsigned char*)AES_KEY_TEXT, 128, &enc_key);
+    for (int i = 0; i < len; i += 16) {
+        AES_encrypt(plaintext + i, ciphertext + i, &enc_key);
+    }
+}
 
-    for(int i = 0; i < len; i++)
-    {
-        data[i] = data[i] ^ key[i % key_len];
+void aes_decrypt(unsigned char *ciphertext, int len, unsigned char *plaintext) {
+    AES_KEY dec_key;
+    AES_set_decrypt_key((unsigned char*)AES_KEY_TEXT, 128, &dec_key);
+    for (int i = 0; i < len; i += 16) {
+        AES_decrypt(ciphertext + i, plaintext + i, &dec_key);
     }
 }
